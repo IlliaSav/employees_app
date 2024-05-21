1fff387a138800142026077595081569cefcfef4
Illia <examle@gmail.com>
2024-05-21 17:54:18 +0300

    Create UI structure

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employees in company N</h1>
+            <h2>Counter of employees: </h2>
+            <h3>Award will be granted: </h3>
+        </div>
+    )
+}
+
+export default AppInfo;
