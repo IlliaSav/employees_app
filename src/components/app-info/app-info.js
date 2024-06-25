4e5a1b867df2332050f31fabf56b9c6c3b4ff406
Illia <examle@gmail.com>
2024-06-25 16:30:37 +0300

    Add increase and rise toggles

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({increased, employees}) => {
     return (
         <div className="app-info">
             <h1>Employees in company N</h1>
-            <h2>Counter of employees: </h2>
-            <h3>Award will be granted: </h3>
+            <h2>Counter of employees: {employees}</h2>
+            <h3>Award will be granted: {increased}</h3>
         </div>
     )
 }
