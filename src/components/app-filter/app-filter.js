add16b6c8402122fb39cf727195148099719c2f0
Illia <examle@gmail.com>
2024-06-26 13:21:30 +0300

    Add search and filter

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,23 +1,29 @@
 import './app-filter.css'
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+    const buttonsData = [
+        {name: 'all', label: 'All employees'},
+        {name: 'rise', label: 'Award'},
+        {name: 'moreThen1000', label: 'More then 1000$'}
+    ];
+
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light'
+        return (
+            <button 
+                className={`btn ${clazz}`}
+                type="button"
+                key={name}
+                onClick={() => props.onFilterSelect(name)}>
+                    {label}
+            </button>
+        )
+    })
+
     return (
         <div className="btn-group">
-            <button 
-                className="btn btn-light"
-                type="button">
-                    All employees
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    Award
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    More then 1000$
-            </button>
+            {buttons}
         </div>
     )
 }
